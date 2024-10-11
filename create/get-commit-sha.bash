e844109cc5602f96c404cccc80ba71df94c18845
Simon Jagoe <simon.jagoe@smartly.io>
2024-10-11 13:54:43 +0300

    Fix shellcheck failures and remove silly templating

--- create/get-commit-sha.bash
+++ create/get-commit-sha.bash
@@ -3,8 +3,8 @@ set -eu -o pipefail
 if [ -n "$COMMIT_STATUS_SHA" ]; then
   SHA="$COMMIT_STATUS_SHA"
 elif [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
-  SHA="${{ github.event.pull_request.head.sha }}"
+  SHA="$PULL_REQUEST_SHA"
 else
-  SHA="${{ github.sha }}"
+  SHA="$GITHUB_SHA"
 fi
 echo sha="$SHA" >> "$GITHUB_OUTPUT"
