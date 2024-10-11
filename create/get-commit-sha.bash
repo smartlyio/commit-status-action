05b02a09a607cd2bfbe5ddb0e20e0355ea9a2140
Simon Jagoe <simon.jagoe@smartly.io>
2024-10-11 10:19:57 +0300

    Update templates

--- create/get-commit-sha.bash
+++ create/get-commit-sha.bash
@@ -1,6 +1,8 @@
 set -eu -o pipefail
 
-if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
+if [ -n "$COMMIT_STATUS_SHA" ]; then
+  SHA="$COMMIT_STATUS_SHA"
+elif [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
   SHA="${{ github.event.pull_request.head.sha }}"
 else
   SHA="${{ github.sha }}"
